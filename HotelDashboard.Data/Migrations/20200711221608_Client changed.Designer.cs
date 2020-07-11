3f455816eab2e0b3758ee7ba3f65b7a6cace74ea
Klishin Ilya <klishin-ilya@mail.ru>
2020-07-12 01:17:14 +0300

    Client model changed

--- HotelDashboard.Data/Migrations/20200711221608_Client changed.Designer.cs
+++ HotelDashboard.Data/Migrations/20200711221608_Client changed.Designer.cs
@@ -4,14 +4,16 @@ using HotelDashboard.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelDashboard.Data.Migrations
 {
     [DbContext(typeof(HotelContext))]
-    partial class HotelContextModelSnapshot : ModelSnapshot
+    [Migration("20200711221608_Client changed")]
+    partial class Clientchanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HotelDashboard.Data/Migrations/20200711221608_Client changed.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelDashboard.Data.Migrations
+{
+    public partial class Clientchanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_RoomId",
+                table: "Client",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Client_Rooms_RoomId",
+                table: "Client",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Client_Rooms_RoomId",
+                table: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Client_RoomId",
+                table: "Client");
+        }
+    }
+}
