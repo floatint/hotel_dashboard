8b2d60faf4a5b231be3e1a6c13a787284f0ce440
Klishin Ilya <klishin-ilya@mail.ru>
2020-07-19 19:05:45 +0300

    Reservation dialog init

---
+++ HotelDashboard.WPFClient/ViewModels/Dialogs/IDialogViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelDashboard.WPFClient.ViewModels.Dialogs
+{
+    /// <summary>
+    /// Интерфейс ViewModel для всех диалогов
+    /// </summary>
+    interface IDialogViewModel
+    {
+        /// <summary>
+        /// Результат диалога
+        /// </summary>
+        object Result { get; }
+    }
+}
