fbfea0dcbd69c7c83770a65c48981a07a6426c1f
Klishin Ilya <klishin-ilya@mail.ru>
2020-07-19 19:03:22 +0300

    Services init

---
+++ HotelDashboard.WPFClient/Services/DialogService.cs
@@ -0,0 +1,34 @@
+﻿using HotelDashboard.WPFClient.ViewModels;
+using HotelDashboard.WPFClient.ViewModels.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace HotelDashboard.WPFClient.Services
+{
+    /// <summary>
+    /// Реализация сервиса диалогов
+    /// </summary>
+    class DialogService : IDialogService
+    {
+
+        public object InputDialog<TView, TViewModel>(string title) where TViewModel : BaseViewModel, IDialogViewModel, new() 
+                                                                   where TView : Window, new()
+        {
+            // создание представления и логики
+            TViewModel viewModel = new TViewModel();
+            TView view = new TView();
+            // подключение логики
+            view.DataContext = viewModel;
+            // показываем диалог и возвращаем введенные данные
+            view.ShowDialog();
+            return viewModel.Result;
+        }
+
+        public void ShowMessage(string title, string message)
+        {
+            MessageBox.Show(message, title);
+        }
+    }
+}
---
+++ HotelDashboard.WPFClient/Services/IDialogService.cs
@@ -0,0 +1,30 @@
+﻿using HotelDashboard.WPFClient.ViewModels;
+using HotelDashboard.WPFClient.ViewModels.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace HotelDashboard.WPFClient.Services
+{
+    /// <summary>
+    /// Интерфейс сервисов диалогов
+    /// </summary>
+    interface IDialogService
+    {
+        /// <summary>
+        /// Просто вывод сообщения пользователю
+        /// </summary>
+        /// <param name="title">Заголовок</param>
+        /// <param name="message">Сообщение</param>
+        void ShowMessage(string title, string message);
+        /// <summary>
+        /// Вызывает диалог и отдает введенные пользователем данные
+        /// </summary>
+        /// <typeparam name="TView">Тип представления</typeparam>
+        /// <typeparam name="TViewModel">Тип логики представления</typeparam>
+        /// <param name="title">Заголовок</param>
+        object InputDialog<TView, TViewModel>(string title) where TViewModel : BaseViewModel, IDialogViewModel, new()
+                                                            where TView : Window, new();
+    }
+}
